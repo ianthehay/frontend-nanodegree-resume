b78b75853c7a131fd31b2f2e41ffbee3e34fc7c0
Ian <Ian@unknown-28-cf-e9-52-a5-b1.home>
2015-06-14 23:11:40 +0100

    L1
    Stuff

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,8 @@
-$("#main").append("Ian Hay");
+//$("#main").append("Ian Hay");
+
+var formattedName = HTMLheaderName.replace("%data%", "Ian Hay")
+var formattedRole = HTMLheaderRole.replace("%data%", "Lord of Aceness")
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
