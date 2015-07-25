ac45a44b242cee10f025500e3ec485b705a6bd27
Ian <Ian@unknown-28-cf-e9-52-a5-b1.home>
2015-07-25 13:33:32 +0100

    More Lesson 1
    Almost done lesson 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,55 @@
 //$("#main").append("Ian Hay");
 
-var formattedName = HTMLheaderName.replace("%data%", "Ian Hay")
-var formattedRole = HTMLheaderRole.replace("%data%", "Lord of Aceness")
+var formattedName = HTMLheaderName.replace("%data%", "Ian Hay");
+var formattedRole = HTMLheaderRole.replace("%data%", "Lord of Aceness");
+var formattedPic= HTMLbioPic.replace("%data%", "images/fry.jpg");
+var formattedSkills = HTMLskills.replace("%data%",["Aceness","Rocking","Coding"]);
+var formattedWelcome = HTMLwelcomeMsg.replace("%data%","Ahoy there!");
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+var bio ={
+	"name": formattedName,
+	"role": formattedRole,
+	"skills":formattedSkills,
+	"welcome" : formattedWelcome,
+	"pic" : formattedPic
+
+};
+
+var work ={};
+work.Start = HTMLworkStart;
+work.Employer = HTMLworkEmployer.replace("%data%","DAI")
+work.Title = HTMLworkTitle.replace("%data%","Software Engineer");
+work.Dates = HTMLworkDates.replace("%data%","August 2010 - Present");
+
+
+var education = {};
+education["Start"]=HTMLschoolStart;
+education["Name"]=HTMLschoolName.replace("%data%","University of Aberdeen");
+education["Degree"]=HTMLschoolDegree.replace("%data%","Masters of Engineering");
+education["Dates"]=HTMLschoolDates.replace("%data%","September 2005 - July 2010");
+education["Major"]=HTMLschoolMajor.replace("%data%","Electrical and Electronic Engineering");
+
+
+$("#header").prepend(bio.role);
+$("#header").prepend(bio.name);
+$("#header").append(bio.pic);
+
+//$("#main").append(bio.name);
+//$("#main").append(bio.role);
+$("#header").append(bio.welcome);
+
+$("#header").append(bio.skills);
+
+$("#workExperience").append(work.Start);
+$("#workExperience").append(work.Employer);
+$("#workExperience").append(work.Title);
+$("#workExperience").append(work.Dates);
+
+$("#education").append(education.Start);
+$("#education").append(education.Name);
+$("#education").append(education.Degree);
+$("#education").append(education.Dates);
+$("#education").append(education.Major);
+
+
 
